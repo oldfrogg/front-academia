e188c86e1c619740b0b9871e3f5b5df881aadd8d
Jhonatta Tavares <130725017+oldfrogg@users.noreply.github.com>
2023-09-21 01:26:49 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -12,7 +12,12 @@ const inNovoTelefone = form.querySelector("#inTelefone");
 const inFiltro = document.querySelector("#inFiltro");
 
 
-// Botao para mostrar o formulário de inserção de novo aluno
+
+/*
+*****************************************************************
+=== Botao para mostrar o formulário de inserção de novo aluno ===
+*****************************************************************
+*/
 bt_novoAluno.addEventListener("click", () => {
     if (getComputedStyle(secaoNovoAluno).getPropertyValue("display") == "flex") {
         secaoNovoAluno.style.setProperty('display', 'none');
@@ -23,7 +28,12 @@ bt_novoAluno.addEventListener("click", () => {
     };
 });
 
-// Botao para mostrar a tabela com a lista de alunos
+
+/*
+*********************************************************
+=== Botao para mostrar a tabela com a lista de alunos ===
+*********************************************************
+*/
 bt_mostraLista.addEventListener("click", () => {
     if (getComputedStyle(secaoTabela).getPropertyValue("display") == "flex") {
         secaoTabela.style.setProperty('display', 'none');
@@ -37,35 +47,38 @@ bt_mostraLista.addEventListener("click", () => {
 
 
 
-// Funcao que inicia no carregamento da pagina e quando ha atualizacoes no BD, que busca a tabela no banco de dados e chama a funcao que monta a lista
+/*
+************************************************************************************
+=== Funcao que inicia no carregamento da pagina e quando ha atualizacoes no BD,  ===
+=== que busca a tabela no banco de dados e chama a funcao que monta a lista      ===
+************************************************************************************
+*/
 async function getList() {
     let url = "http://127.0.0.1:5000/get_alunos";
     fetch(url, { method: 'GET' })
-        .then(response => {
-            return response.json();
+        .then(resposta => {
+            return resposta.json();
         })
         .then(data => {
-            corpoTabelaAlunos.innerHTML = ""; // limpa a tabela
+            limpaTabela();
             data.alunos.forEach(aluno => insertList(aluno.matricula, aluno.cpf, aluno.nome, aluno.telefone, aluno.validade));
-            editaCadastro(".edita");
-            contrataPlano(".contrata");
-            excluiAluno(".exclui");
+            aplicaOuvintes()
             // faco os ouvintes das listas de botoes de edicao, contratacao e exclusao de alunos da tabela
         })
         .catch((error) => {
             console.error('Error:', error);
         });
-
-    inNovoAluno.value = "";
-    inNovoCPF.value = "";
-    inNovoTelefone.value = "";
-    inFiltro.value = "";
+    limpaCampos();
 };
 getList();
 
 
 
-// Funcao que mostra a lista ao usuario
+/*
+********************************************
+=== Funcao que mostra a lista ao usuario ===
+********************************************
+*/
 function insertList(matricula, cpf, nome, tel, validade) {
     let aluno = [matricula, cpf, nome, tel, validade];
     const linha = corpoTabelaAlunos.insertRow();
@@ -75,13 +88,19 @@ function insertList(matricula, cpf, nome, tel, validade) {
         let cel = linha.insertCell(i);
         cel.textContent = aluno[i];
     };
+
+    // Chama a função para inserir nas ultimsa colunas os botões de edição, adição de plano e exclusão do aluno.
     insertButton(linha.insertCell(-1), "✎", "edita");
     insertButton(linha.insertCell(-1), "+", "contrata");
     insertButton(linha.insertCell(-1), "\u00D7", "exclui");
 };
 
 
-// Funcao chamada por insertList, que coloca os botoes de edita e excluir ultimas colunas de cada linha
+/*
+************************************************************************************************************
+=== Funcao chamada por insertList, que coloca os botoes de edita e excluir ultimas colunas de cada linha ===
+************************************************************************************************************
+*/
 function insertButton(parent, caractere, classe) {
     let botao = document.createElement("button");
     let txt = document.createTextNode(caractere);
@@ -91,9 +110,26 @@ function insertButton(parent, caractere, classe) {
 };
 
 
-// Funcao para filtrar aluno por CPF
+/*
+*********************************************************************************************************
+=== Funcao auxiliar que ativa os botões de alteracao cadastral, inclusao de plano e exclusao de aluno ===
+*********************************************************************************************************
+*/
+async function aplicaOuvintes() {
+    editaCadastro(".edita");
+    contrataPlano(".contrata");
+    excluiAluno(".exclui");
+}
 
-// Capta o valor do CPF e chama a funcao que chama a rota de filtro /get_aluno
+
+
+/*
+***********************************************************************************
+                === Funcao para filtrar aluno por CPF ===
+***********************************************************************************
+=== Capta o valor do CPF e chama a funcao que chama a rota de filtro /get_aluno ===
+***********************************************************************************
+*/
 function filtraAlunos() {
     bt_filtrar.addEventListener("click", () => {
         let cpf = inFiltro.value;
@@ -102,46 +138,37 @@ function filtraAlunos() {
             getList()
             return; //caso o campo esteja vazio, mostra a lista completa
         }
-        getListFiltrada(cpf)
-        inFiltro.value = ""
-        inFiltro.focus()
+        getListFiltrada(cpf);
+        inFiltro.value = "";
+        inFiltro.focus();
     });
 };
 filtraAlunos();
 
 
-// Utiliza a rota e obtem o retorno. Chama a funcao que deixa apenas o conteudo filtrado no Front
-function getListFiltrada(cpf) {
+/*
+******************************************************************************************************
+=== Utiliza a rota e obtem o retorno. Chama a funcao que deixa apenas o conteudo filtrado no Front ===
+******************************************************************************************************
+*/
+async function getListFiltrada(cpf) {
     let url = `http://127.0.0.1:5000/get_aluno?cpf=${cpf}`;
     fetch(url, { method: 'GET' })
         .then(resposta => resposta.json())
         .then((dados) => {
-            corpoTabelaAlunos.innerHTML=""; // Limpa a tabela
+            limpaTabela();
             insertList(dados.matricula, dados.cpf, dados.nome, dados.tel, dados.validade);
         })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+        .then(() => aplicaOuvintes())
+        .catch((error) => console.error('Error:', error));
 };
 
 
-// Funcao de apoio ao filtro de aluno. Nao mexe diretamente no Banco de Dados.
-function removeDiferentes(matricula) {
-    const linhas = document.querySelectorAll('tr');
-    let linhas_matricula = linhas[1].childNodes[0].innerText
-    for (i = 1; i < linhas.length; i++) {
-        linhas_matricula = linhas[i].childNodes[0].innerText
-        if (matricula != linhas_matricula) {
-            linhas[i].remove();
-        }
-    };
-};
-
-
-
-
-
-// Funcao para adicionar Aluno
+/*
+***********************************
+=== Funcao para adicionar Aluno ===
+***********************************
+*/
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const formData = new FormData();
@@ -160,24 +187,30 @@ form.addEventListener("submit", (e) => {
                 throw new Error("Erro ao adicionar novo aluno.");
             };
         })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+        .catch((error) => console.error('Error:', error));
 });
 
 
 
 
-// Funcoes chamadas no getList que adiciona as classes nos botoes de edicao e exclusao, junto com os ouvintes de evento
 
-//Funcao que ira editar o cadastro de um aluno - Nome e Telefone
+/*
+*****************************************************************************************************************************
+
+=== Funcoes chamadas no getList que adicionam as classes nos botoes de edicao e exclusao, junto com os ouvintes de evento ===
+
+*****************************************************************************************************************************
+*/
+/*
+**********************************************************************
+=== Funcao que ira editar o cadastro de um aluno - Nome e Telefone ===
+**********************************************************************
+*/
 function editaCadastro(classe) {
     const bt_editaAluno = document.querySelectorAll(classe);
     // Itera sobre cada elemento e adiciona o event listener
     bt_editaAluno.forEach((element, index) => {
         element.addEventListener("click", (e) => {
-            console.log("Clicou no elemento:", element, "index:", index);
-            console.log("Evento:", e);
             const linha_atual = element.parentElement.parentElement;
             const cpf = linha_atual.children[1].innerText;
             const nome = prompt(`Insira o nome correto do Aluno \n CPF: ${cpf}`);
@@ -193,23 +226,23 @@ function editaCadastro(classe) {
                                 alert("Dados do aluno atualizados com sucesso!")
                             }
                             else {
-                                throw new Error("Erro ao atualizar dados do aluno.");
+                                throw new Error("Erro ao atualizar dados do aluno.")
                             }
                         })
-                        .then(data => {
-                            getList();
-                            console.log('Resposta:', data);
-                        })
-                        .catch((error) => {
-                            console.error('Erro:', error);
-                        });
+                        .then(data => getList())
+                        .catch((error) => console.error('Erro:', error));
                 }
             }
         });
     });
 };
 
-// Funcao que ira adicionar X meses a validade do plano do cliente
+
+/*
+***********************************************************************
+=== Funcao que ira adicionar X meses a validade do plano do cliente ===
+***********************************************************************
+*/
 function contrataPlano(classe) {
     const bt_contrata = document.querySelectorAll(classe);
     // Itera sobre cada elemento e adiciona o event listener
@@ -218,6 +251,7 @@ function contrataPlano(classe) {
             const linha_atual = element.parentElement.parentElement;
             const cpf = linha_atual.children[1].innerText;
             let qtd_meses = prompt("Quantos meses o aluno contratou?");
+            if (qtd_meses == null) { return } // Para a execução em caso de não inclusão de dado
             const parametros = { cpf: cpf, qtd_meses: qtd_meses };
             let url = 'http://127.0.0.1:5000/contrata_plano';
             fetch(`${url}?${new URLSearchParams(parametros).toString()}`, { method: 'PUT' })
@@ -230,36 +264,60 @@ function contrataPlano(classe) {
                         throw new Error("Erro ao contratar plano.");
                     }
                 })
-                .then(data => {
-                    getList();
-                    console.log('Resposta:', data);
-                })
-                .catch((error) => {
-                    console.error('Erro:', error);
-                });
+                .then(data => { getList() })
+                .catch((error) => { console.error('Erro:', error) });
         });
     });
 };
 
-// Funcao que excui o cadastro do aluno
+/*
+********************************************
+=== Funcao que excui o cadastro do aluno ===
+********************************************
+*/
 function excluiAluno(classe) {
     const bt_exclui = document.querySelectorAll(classe);
     // Itera sobre cada elemento e adiciona o event listener
     bt_exclui.forEach((element, index) => {
         element.addEventListener("click", (e) => {
-            console.log("Clicou no elemento:", element, "index:", index);
-            console.log("Evento:", e);
             if (confirm("Você tem certeza?")) {
                 const linha_atual = element.parentElement.parentElement;
                 const cpf = linha_atual.children[1].innerText;
                 let url = "http://127.0.0.1:5000/del_aluno?cpf=" + cpf;
                 fetch(url, { method: 'DELETE' })
                     .then((response) => response.json())
-                    .catch((error) => {
-                        console.error('Error:', error);
-                    });
-                linha_atual.remove();
+                    .then(() => linha_atual.remove())
+                    .catch((error) => console.error('Error:', error));
             };
         });
     });
 };
+
+
+
+
+
+
+
+
+
+/*
+*******************************************************************************************
+=== Funcao que limpa a tabela, para ser chamada antes de construir, evitando duplicacao ===
+*******************************************************************************************
+*/
+function limpaTabela() {
+    corpoTabelaAlunos.innerHTML = "";
+}
+
+/*
+*********************************************************************************************
+=== Funcao que limpa os campos de preenchimento, para nao ter nada ao recarregar a pagina ===
+*********************************************************************************************
+*/
+function limpaCampos() {
+    inNovoAluno.value = "";
+    inNovoCPF.value = "";
+    inNovoTelefone.value = "";
+    inFiltro.value = "";
+}
